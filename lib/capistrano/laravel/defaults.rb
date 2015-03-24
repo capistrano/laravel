9f46623c10ab4027097e8ad8cf84eae0270e7144
Andrew Miller <andrew.miller@mail.rakuten.com>
2015-03-24 09:21:23 +0900

    Added vendor folder and storage/app folder.

--- lib/capistrano/laravel/defaults.rb
+++ lib/capistrano/laravel/defaults.rb
@@ -1,31 +1,19 @@
 set :laravel_roles, :all
 set :laravel_artisan_flags, "--env=production"
 set :laravel_server_user, "www-data"
-set :laravel_version, 5
 
 # fix bug in capistrano-file-permissions
 set :linked_dirs, []
 
-# Use Laravel 5 paths by default
 set :file_permissions_paths, [
   'storage',
+  'storage/app',
   'storage/framework/cache',
   'storage/framework/sessions',
   'storage/framework/views',
   'storage/logs',
+  'vendor',
 ]
-
-# Load Laravel 4 paths if requested
-if :laravel_version == 4
-  set :file_permissions_paths, [
-    'app/storage',
-    'app/storage/cache',
-    'app/storage/logs',
-    'app/storage/meta',
-    'app/storage/sessions',
-    'app/storage/views',
-  ]
-end
 
 set :file_permissions_users, [fetch(:laravel_server_user)]
 
